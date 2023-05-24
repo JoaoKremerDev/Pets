09421a242295c9e822d70eee60210b67b9452b16
JoaoKremerDev <joaokremer.developer@gmail.com>
2023-05-24 19:51:54 -0300

    feat: userLogin implemented at the LoginForm component

--- app/src/Components/Header.jsx
+++ app/src/Components/Header.jsx
@@ -1,24 +1,31 @@
-import '../App.css'
-import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
-import Dogs from '../Assets/dogs.svg';
-import {UserContext} from '../Context/useContext';
-import { useContext } from 'react';
-
+import "../App.css";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import Dogs from "../Assets/dogs.svg";
+import { UserContext } from "../Context/useContext";
+import { useContext } from "react";
 
 const Header = () => {
-  const { data } = useContext(UserContext)
+  const { data } = useContext(UserContext);
 
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to='/' aria-label="Pets - Home">
+        <Link className={styles.logo} to="/" aria-label="Pets - Home">
           <img src={Dogs} alt="" />
         </Link>
-        <Link className={styles.login} to="/login">Login / Criar</Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
