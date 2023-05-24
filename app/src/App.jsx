d0c3543c073d8c8bfd8717d91c48439b912fc180
JoaoKremerDev <joaokremer.developer@gmail.com>
2023-05-24 11:40:38 -0300

    feat: Login component and route created

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from './Components/Home';
 import {BrowserRouter, Route, Routes, } from "react-router-dom";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
             <Route path='/' element={<Home />}/>
+            <Route path="/login" element={<Login/>}/>
       </Routes>
       <Footer />
       </BrowserRouter>
