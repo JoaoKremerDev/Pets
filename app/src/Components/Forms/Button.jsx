a0e0ae3357d535e2cff365de0d86f151d12df42b
JoaoKremerDev <joaokremer.developer@gmail.com>
2023-05-24 16:34:08 -0300

    style: login form styled

--- app/src/Components/Forms/Button.jsx
+++ app/src/Components/Forms/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import styles from './Button.module.css'
 
-const Button = ({children}) => {
+const Button = ({children, ...props}) => {
   return (
-      <button className={styles.button}>{children}</button>
+      <button {...props}className={styles.button}>{children}</button>
   )
 }
 
