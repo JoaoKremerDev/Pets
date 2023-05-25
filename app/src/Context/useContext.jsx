269edb559e0211caea5c1821a8f1d01d6bf95ad4
JoaoKremerDev <joaokremer.developer@gmail.com>
2023-05-25 10:56:56 -0300

    fix: userContext => UserEffect missing dependencie and added useCallBack to userLogin

--- app/src/Context/useContext.jsx
+++ app/src/Context/useContext.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from "../api";
@@ -11,6 +11,46 @@ export const UserStorage = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+
+  const userLogout = useCallback(
+    async function () {
+      setData(null);
+      setError(null);
+      setLoading(false);
+      setLogin(false);
+      window.localStorage.removeItem("token");
+      navigate("/login");
+    },
+    [navigate]
+  );
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    setData(json);
+    setLogin(true);
+  }
+
+  async function userLogin(username, password) {
+    try {
+      setError(null);
+      setLoading(true);
+      const { url, options } = TOKEN_POST({ username, password });
+      const tokenRes = await fetch(url, options);
+      if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`);
+      console.log(tokenRes);
+      const { token } = await tokenRes.json();
+      window.localStorage.setItem("token", token);
+      await getUser(token);
+      navigate("/conta");
+    } catch (err) {
+      setError(err.message);
+      setLogin(false);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
     async function autoLogin() {
@@ -31,47 +71,12 @@ export const UserStorage = ({ children }) => {
       }
     }
     autoLogin();
-  }, []);
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    setData(json);
-    setLogin(true);
-  }
-
-  async function userLogin(username, password) {
-    try {
-      setError(null);
-      setLoading(true);
-      const { url, options } = TOKEN_POST({ username, password });
-      const tokenRes = await fetch(url, options);
-      if(!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`)
-      console.log(tokenRes)
-      const { token } = await tokenRes.json();
-      window.localStorage.setItem("token", token);
-       await getUser(token);
-       navigate('/conta')
-    } catch (err) {
-      setError(err.message)
-      setLogin(false);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function userLogout() {
-    setData(null);
-    setError(null);
-    setLoading(false);
-    setLogin(false);
-    window.localStorage.removeItem("token");
-    navigate('/login')
-  }
+  }, [userLogout]);
 
   return (
-    <UserContext.Provider value={{ userLogin, data, userLogout, error, loading, login }}>
+    <UserContext.Provider
+      value={{ userLogin, data, userLogout, error, loading, login }}
+    >
       {children}
     </UserContext.Provider>
   );
