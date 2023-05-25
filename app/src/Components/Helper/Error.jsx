fe4e2a38854593faabf2f953508a8b8181600f23
JoaoKremerDev <joaokremer.developer@gmail.com>
2023-05-25 12:18:24 -0300

    fix: Error Componet (conditional structure)

--- app/src/Components/Helper/Error.jsx
+++ app/src/Components/Helper/Error.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Error = ({error}) => {
+const Error = ({ error }) => {
+  if (!error) return null;
+  
   return (
     <div>
-      if(!error) return null;
-      return (
-        <p style={{color: '#f31', margin: '1rem 0'}}>{error}</p>
-      )
+      <p style={{ color: '#f31', margin: '1rem 0' }}>{error}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
